4cc9df77e87c90494b97ec1a801acc480ff3ca22
algoaryan <algoaryan@gmail.com>
2024-04-20 20:05:30 +0530

    Day 01

---
+++ gridbox/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="box1">Box 1</div>
+        <div className="box2">Box 2</div>
+        <div className="box3">Box 3</div>
+        <div className="box4">Box 4</div>
+        <div className="box5">Box 5</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
